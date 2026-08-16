f04d448dd6bff9e888efc6a935820e709b93e971
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-08-16 17:46:53 -0500

    Enemy Health Generator

---
+++ functions.py
@@ -0,0 +1,5 @@
+import random
+
+def enemy_health():
+    enemy_health = random.randint(1, 20)
+    return enemy_health
---
+++ main.py
@@ -0,0 +1,4 @@
+from functions import *
+
+enemy_health = enemy_health()
+print (enemy_health)
