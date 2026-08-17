b5d377887c387853e5dea2374489213f4f65611d
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-08-16 21:13:49 -0500

    Added Enemy Stats

--- functions.py
+++ functions.py
@@ -3,3 +3,11 @@ import random
 def enemy_health():
     enemy_health = random.randint(10, 20)
     return enemy_health
+
+def enemy_atk():
+    enemy_atk = random.randint(1, 20)
+    return enemy_atk
+
+def enemy_def():
+    enemy_def = random.randint(1, 20)
+    return enemy_def
