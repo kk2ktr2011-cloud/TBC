3c297764d10fa61cbfc9b9f6665a113771dc3314
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-08-16 21:23:21 -0500

    Added Battle Code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functions import *
+import time
 
 enemy_health = enemy_health()
 enemy_atk = enemy_atk()
@@ -8,6 +9,22 @@ mc_health = int(input("What is your health (1-20): "))
 mc_atk = int(input("What is your attack power (1-10)"))
 mc_def = int(input("What is your defense (1-10): "))
 
+time.sleep(1)
+
 print(f"This is the enemies health: {enemy_health}.")
 print(f"This is the enemies attack power: {enemy_atk}.")
 print(f"This is the enemies defense: {enemy_def}.")
+
+time.sleep(5)
+
+print("The battle starts.\n")
+
+while mc_health > 0 and enemy_health > 0:
+    mc_attack = mc_atk - enemy_def
+    if mc_attack < 0:
+        mc_attack == 0
+        mc_atk += 1
+        
+    enemy_health -= mc_attack
+    print(f"You attacked by {mc_attack} and the enemies health is now {enemy_health}.")
+    
