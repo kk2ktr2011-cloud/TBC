375c21282cc699eb7c76c16b5cc561fb4f31e482
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-08-16 21:15:32 -0500

    Added Printing of All Enemy Statistics

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from functions import *
 
 enemy_health = enemy_health()
+enemy_atk = enemy_atk()
+enemy_def = enemy_def()
 
 mc_health = int(input("What is your health (1-20): "))
 mc_atk = int(input("What is your attack power (1-10)"))
 mc_def = int(input("What is your defense (1-10): "))
 
-print(f"This is the enemies health: {enemy_health}")
+print(f"This is the enemies health: {enemy_health}.")
+print(f"This is the enemies attack power: {enemy_atk}.")
+print(f"This is the enemies defense: {enemy_def}.")
